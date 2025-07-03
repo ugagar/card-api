04f229580f0a4324d2dfeb60630c6c33d48eff7b
Danil Ugarov <ugarovd06@gmail.com>
2025-07-04 01:50:43 +0300

    add '/cards/all' endpoint end constants

---
+++ api.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from models import CardSuit
+
+suits = [CardSuit.clubs, CardSuit.diamonds, CardSuit.hearts, CardSuit.spades]
+values = range(6, 14)
+
+app = FastAPI()
+
+@app.get("/cards/all")
+async def read_all_cards():
+    return [
+        {"value": value, "suit": suit}
+        for value in values
+        for suit in suits
+    ]
