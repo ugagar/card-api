8d4d86c0d205bf3bd6a94396b5ec0e8b8559b32d
Danil Ugarov <ugarovd06@gmail.com>
2025-07-04 01:34:47 +0300

    Add Enum CardSuit

--- models.py
+++ models.py
@@ -1,5 +1,12 @@
+from enum import Enum
 from pydantic import BaseModel
+
+class CardSuit(str, Enum):
+    hearts = "hearts"
+    spades = "spades"
+    diamonds = "diamonds"
+    clubs = "clubs"
 
 class Card(BaseModel):
     value: int
-    suit: str
+    suit: CardSuit
