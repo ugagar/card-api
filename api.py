9db097a9d2351c32c4977c94b7e65b77083e7d2b
Danil Ugarov <ugarovd06@gmail.com>
2025-07-05 01:55:51 +0300

    Add /dack/shuffle endpoint

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from random import shuffle as rand_shuffle
 
 from constants import CARD_SUITS, CARD_VALUES
 from models import Card
@@ -6,14 +7,26 @@ from models import Card
 
 app = FastAPI()
 
+deck = []
+
 @app.get("/cards/all")
 async def read_all_cards():
-    return [
+    global deck
+    deck = [
         {"value": value, "suit": suit}
         for value in CARD_VALUES
         for suit in CARD_SUITS
     ]
+    return deck
 
 @app.put("/cards/create")
 async def create_card(card: Card):
     return card
+
+@app.post("/deck/shuffle")
+async def shuffle_deck():
+    rand_shuffle(deck)
+    return {
+        "status": "Deck shuffled!",
+        "deck": deck
+        }
