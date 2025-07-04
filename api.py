28269c79e90e76d50acd5253ec9cd6c8f5040615
Danil Ugarov <ugarovd06@gmail.com>
2025-07-05 02:12:29 +0300

    Use DEFOULT_DECK in endpoints

--- api.py
+++ api.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from random import shuffle as rand_shuffle
 
-from constants import CARD_SUITS, CARD_VALUES
-from models import Card, Deck
+from models import Card, Deck, DEFOULT_DECK
 
 
 app = FastAPI()
 
 @app.get("/deck")
-async def read_all_cards(deck: Deck):
+async def read_all_cards(deck: Deck = DEFOULT_DECK):
     return deck
 
 @app.put("/cards/create")
@@ -16,7 +15,7 @@ async def create_card(card: Card):
     return card
 
 @app.post("/deck/shuffle")
-async def shuffle_deck(deck: Deck):
+async def shuffle_deck(deck: Deck = DEFOULT_DECK):
     rand_shuffle(deck.cards)
     return {
         "status": "Deck shuffled!",
